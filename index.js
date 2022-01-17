50b2cd698ef8ac988fa080f478766bd03dddf356
Fritiof Rusck <fritiof@bjornssonrusck.se>
2022-01-17 20:06:23 +0100

    fix: heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
